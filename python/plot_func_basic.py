a0adb7f376ce43453ebc1247e0442a20d7c4d39d
Quinton Lai <20620907+quintonlai@users.noreply.github.com>
2019-08-05 20:11:24 +0800

    Update plot_func_basic.py

--- python/plot_func_basic.py
+++ python/plot_func_basic.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-def hist_plot(var, title_txt, xlab_txt, ylab_txt, bar_color):
+def hist_plot(df_name, var, title_txt, xlab_txt, ylab_txt, bar_color):
     fig = plt.figure()
     plt.title(title_txt)
     plt.xlabel(xlab_txt)
     plt.ylabel(ylab_txt)
-    plt.hist(zwift_test_ds[var], color = bar_color)
+    plt.hist(df_name[var], color = bar_color)
     
 #Example
 #hist_plot('Distance', 'Distance', 'km', 'Frequency')
