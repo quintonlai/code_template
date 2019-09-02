d6f601fafc667e71fb377abe82e672675f3355e0
Quinton Lai <20620907+quintonlai@users.noreply.github.com>
2019-09-02 19:01:15 +0800

    Update data_explore.py

--- python/data_explore.py
+++ python/data_explore.py
@@ -15,5 +15,13 @@ def plot_mean_compare(df_name, grp_var, y_var, title_txt):
     plt.title('Boxplot of '+ y_var + ' by ' + grp_var )
     sns.boxplot(grp_var,y_var, data = df_name)
     plt.show()
-    
+
+'''
+plot_mean_compare
+    df_name: data frame name
+    grp_var: group by variable
+    y_var: target (numeric) variable
+    title_txt: Title
+'''
+# Example
 plot_mean_compare(data, 'Gender', 'Purchase', 'Average purchased amount by Gender')
