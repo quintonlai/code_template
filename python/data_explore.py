554f553ecd280465986533ad6cff0beb9806b404
Quinton Lai <20620907+quintonlai@users.noreply.github.com>
2019-09-02 18:54:14 +0800

    Update data_explore.py

--- python/data_explore.py
+++ python/data_explore.py
@@ -1,24 +1,19 @@
-# Visualize the difference of purchased amount among gender
-data[['Gender','Purchase']].groupby('Gender').mean().plot.bar()
-sns.barplot('Gender', 'Purchase', data = data)
-plt.show()
+import pandas as pd
+import numpy as np                     
+import seaborn as sns
+import matplotlib.pyplot as plt        
+%matplotlib inline 
 
 
-# Visualize the purchased amount by age group 
-data[['Age','Purchase']].groupby('Age').mean().plot.bar()
-sns.barplot('Age', 'Purchase', data = data)
-plt.show()
-
-# Visualize the distribution of purchased amount by age group
-sns.boxplot('Age','Purchase', data = data)
-plt.show()
-
-# Visualize the difference of purchased amount across the city
-data[['City_Category','Purchase']].groupby('City_Category').mean().plot.bar()
-sns.barplot('City_Category', 'Purchase', data = data)
-plt.show()
-
-# Visualize the difference of purchased amount against years stay in the current city
-data[['Stay_In_Current_City_Years','Purchase']].groupby('Stay_In_Current_City_Years').mean().plot.bar()
-sns.barplot('Stay_In_Current_City_Years', 'Purchase', data = data)
-plt.show()
+# Compare mean value of y in two groups
+def plot_mean_compare(df_name, grp_var, y_var, title_txt):
+    df_name[[grp_var, y_var]].groupby(grp_var).mean().plot.bar()
+    sns.barplot(grp_var, y_var, data = df_name)
+    plt.title(title_txt)
+    plt.show()
+    
+    plt.title('Boxplot of '+ y_var + ' by ' + grp_var )
+    sns.boxplot(grp_var,y_var, data = df_name)
+    plt.show()
+    
+plot_mean_compare(data, 'Gender', 'Purchase', 'Average purchased amount by Gender')
