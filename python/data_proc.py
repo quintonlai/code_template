ed5fb5a162de151a51acbc5bd341946e6fb6defb
Quinton Lai <20620907+quintonlai@users.noreply.github.com>
2019-09-02 18:39:43 +0800

    Create data_proc.py

---
+++ python/data_proc.py
@@ -0,0 +1,15 @@
+from sklearn.model_selection import train_test_split
+
+# Separate x and y matrices
+X_temp = data.drop(['Y_NAME'], axis = 1)
+y_temp = data['Y_NAME']
+
+# Turn into values
+X = X_temp.values
+y = y_temp.values
+
+# Split into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+print(X_train.shape)
+print(X_test.shape)
